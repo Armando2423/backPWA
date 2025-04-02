049ea6c0df7792167df892864dc9afaeb5eee125
Armando2423 <sergio.reyes.21m@utzmg.edu.mx>
2025-04-02 11:57:58 -0600

    up again backend5

--- server.js
+++ server.js
@@ -15,10 +15,7 @@ app.use(cors());          // Permitir solicitudes desde diferentes orígenes
 
 
 // Conectar a MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('✅ Conectado a MongoDB'))
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('✅ Conectado a MongoDB Atlas...'))
   .catch(err => console.error('❌ Error de conexión:', err));
 
 // Rutas
